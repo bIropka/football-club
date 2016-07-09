acea930181eaaa06a32c45b3a6b9f36bb4384e0b
Iegor Stetsiura <biropka@gmail.com>
2016-07-10 02:24:00 +0300

    Initial commit

---
+++ js/custom-scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        pauseOnDotsHover: true
+    });
+
+});
