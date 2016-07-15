8b14ff287e7b97e0258e146f0e5b376987bc384b
Iegor Stetsiura <biropka@gmail.com>
2016-07-15 03:12:38 +0300

    v 0.3

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -100,6 +100,103 @@ $(document).ready(function () {
         pauseOnDotsHover: true
     });
 
+    $('.product-slider').slick({
+        autoplay: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '.left-control',
+        nextArrow: '.right-control'
+    });
+
     /** end of scripts for sliders **/
+
+    /** script for size-chooser **/
+
+        $('.size-chooser ul li').click(function() {
+            if (!$(this).hasClass('active') && !$(this).hasClass('link-item')) {
+                $('.size-chooser ul .active').removeClass('active');
+                $(this).toggleClass('active');
+                $('#size-chooser').val($(this).html());
+            }
+        });
+
+    /** end of script for size-chooser **/
+
+    /** script for color-chooser **/
+
+        $('.color-chooser ul li').click(function() {
+            if (!$(this).hasClass('active')) {
+                $('.color-chooser ul .active').removeClass('active');
+                $(this).toggleClass('active');
+                $('#color-chooser').val(getFirstClassName(this));
+            }
+        });
+
+        function getFirstClassName(item) {
+            var array = item.className.split(' ');
+            return array[0];
+        }
+
+    /** end of script for color-chooser **/
+
+    /** script for product-sliders **/
+
+        $('.product-slider-menu li').click(function() {
+
+            if (!$(this).hasClass('active')) {
+
+                $('.product-slider-menu .active').removeClass('active');
+                $(this).toggleClass('active');
+
+                $('.product-slider-wrapper').removeClass('active');
+
+                switch(getFirstClassName(this)) {
+                    case 'item-for-men':
+                        $('.product-slider-for-men').addClass('active');
+                        $('.product-slider').slick('slickGoTo', 0);
+                        break;
+                    case 'item-for-women':
+                        $('.product-slider-for-women').addClass('active');
+                        $('.product-slider').slick('slickGoTo', 0);
+                        break;
+                    case 'item-for-children':
+                        $('.product-slider-for-children').addClass('active');
+                        $('.product-slider').slick('slickGoTo', 0);
+                        break;
+                }
+            }
+        });
+
+    /** end of script for product-sliders **/
+
+    /** script for menu-bottom **/
+
+        $('.bottom-menu li').click(function() {
+
+            if (!$(this).hasClass('active')) {
+
+                $('.bottom-menu .active').removeClass('active');
+                $(this).toggleClass('active');
+
+                $('.bottom-menu-item').removeClass('active');
+
+                switch(getFirstClassName(this)) {
+                    case 'item-analogs':
+                        $('.analogs').addClass('active');
+                        break;
+                    case 'item-shipping-payment':
+                        $('.shipping-payment').addClass('active');
+                        break;
+                    case 'item-about-product':
+                        $('.about-product').addClass('active');
+                        break;
+                    case 'item-product-reviews':
+                        $('.product-reviews').addClass('active');
+                        break;
+                }
+            }
+        });
+
+    /** end of script for menu-bottom **/
 
 });
